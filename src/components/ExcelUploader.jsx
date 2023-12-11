4d772d77523453027155d9a0ee1b6538cf2d4470
ste519 <qianzi@umich.edu>
2023-12-11 18:27:54 -0500

    Added download template feature to excel uploader

--- src/components/ExcelUploader.jsx
+++ src/components/ExcelUploader.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { postExcelFile } from '../api/post';
 import { useAlertContext } from '../hooks/useCustomContext';
+import api from '../api/axios';
 
 export default function ExcelUploader({ close }) {
     const [file, setFile] = useState(null);
@@ -8,6 +9,22 @@ export default function ExcelUploader({ close }) {
     const handleFile = (event) => {
         setFile(event.target.files[0])
     };
+
+    const downloadTemplate = async () => {
+        try {
+            const response = await api.get('/order/downloadFile')
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', '导入模板.xlsx'); 
+            document.body.appendChild(link);
+            link.click();
+            link.remove()
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
 
     const addData = async () => {
         if (!file) {
@@ -31,8 +48,9 @@ export default function ExcelUploader({ close }) {
     return (
         <div className='excel-uploader-container col flex-center '>
             <input id="excel-uploader" type="file" accept=".xlsx,.xls" onChange={handleFile} className='hidden' />
+            <div><button onClick={downloadTemplate} className='blue40 small'>下载导入模板</button></div>
             <label htmlFor="excel-uploader">选择文件(.xls, .xlsx)</label>
-            <h1>{file?.name}</h1>
+            {file?.name}
             <div className='row flex-center g1'>
                 <button onClick={close} className='white small bordered'>取消</button>
                 <button onClick={addData} className='blue40 small'>导入</button>
